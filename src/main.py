6e5f9247f2119e05dd9ea31b5ca8a5c11c3914cf
sswellington <wellws@pm.me>
2021-04-19 09:07:03 -0300

    docs: login and search

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from Escavador import Auth, Query
 
 
 def login():
+    """ forma de realizar login, sem expor os dados de autentificação """
     l = []    
     f = open('_vault/auth.txt', 'r')
     for line in f:
@@ -20,40 +21,59 @@ if __name__ == "__main__" :
             level=logging.DEBUG, 
             format=' %(asctime)s - %(levelname)s - %(message)s')
     
+    ''' LOGIN : INICIO '''
     EMAIL = ((login())[0]).strip()  # substitua o campo pelo seu email
     PWD = ((login())[1])            # substitua o campo pela sua senha
+    ''' LOGIN : FIM '''
     
+    ''' PESQUISA : INICIO
+        q : O termo a ser pesquisado.
+            Você pode pesquisar entre aspas duplas para match perfeito
+        qo: Tipo da entidade a ser pesquisada    
+            t : Para pesquisar todos os tipos de entidades.
+            p : Para pesquisar apenas as pessoas.
+            i : Para pesquisar apenas as instituições.
+            pa: Para pesquisar apenas as patentes.
+            d : Para pesquisar apenas os Diários Oficiais. 
+    '''
+    PESQUISA = {
+        'q': "Mariza Ferro",   
+        'qo': 'p',
+        # 'limit': '10',  # Número de itens que serão retornados por página, opcional 
+        # 'page': '1'  # Número da página, respeitando o limite informado, opcional
+    }
+    ''' PESQUISA : FIM '''
+    
+    ''' NÃO É NECESSÁRIO ALTERAR AS INFORMAÇÕES ABAIXO '''
     logging.info("Auth Beging")
     
     oauth = Auth(EMAIL, PWD)    
     try: 
         bearer = oauth.bearer()
     except:
-        logging.warning("Error: email ou senha incorreto")
+        logging.warning("Error: email or password incorrect")
         raise("Error: email ou senha incorreto")
     finally:
         del oauth    
         del EMAIL
         del PWD
     
+    print(bearer)
+    
     logging.debug(bearer)
     logging.info("Auth End")
     logging.info("Bearer Beging")
 
-    tatu = Query(bearer)
-    
-    params = {
-        'q': "Mariza Ferro",  # O termo a ser pesquisado. 
-        'qo': 'p',  # Tipo da entidade a ser pesquisada
-        # 'limit': '10',  # Número de itens que serão retornados por página, opcional 
-        # 'page': '1'  # Número da página, respeitando o limite informado, opcional
-    }
-    
-    result = tatu.search_all_data(params)
-    del tatu
-    del params
-    
+    try: 
+        tatu = Query(bearer)    
+    except: 
+        logging.warning("Error: bearer token incorrect")
+        raise("Error: token bearer incorreto")
+        
+    result = tatu.search_all_data(PESQUISA)
     logging.info(result)
+    del tatu
+    del PESQUISA
     
     with open('database/sampling.json', 'a', encoding='utf-8') as f:
         json.dump(result, f, ensure_ascii=False, indent=4)
