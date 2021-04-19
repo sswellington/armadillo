fc50f8713c1be6a1fb4523c384ba1a914334a01c
sswellington <wellws@pm.me>
2021-04-19 11:59:39 -0300

    refactor: sampling

--- src/json_2_csv.py
+++ src/json_2_csv.py
@@ -1,12 +1,15 @@
 import json
 import pandas as pd
 
-PEOPLE = 'Dimas Soares Lima'
-PEOPLE = 'Mariza Ferro'
-PEOPLE = 'Otto Teixeira Fraga Netto'
+PEOPLE = [
+  'Dimas Soares Lima',
+  'Mariza Ferro',
+  'Otto Teixeira Fraga Netto'
+]
 
-with open('database/'+PEOPLE+'.json') as f:
-  data = json.load(f)
+for i in PEOPLE:
+  with open('database/'+i+'.json') as f:
+    data = json.load(f)
 
-df = pd.json_normalize(data["items"])
-df.to_csv('database/'+PEOPLE+'.csv')
+  df = pd.json_normalize(data["items"])
+  df.to_csv('database/'+i+'.csv')
